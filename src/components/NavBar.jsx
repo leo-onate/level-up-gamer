97ff0053ed1103cb17d4d4089e9ce19c52da3e36
Nash <ig.bravom@duocuc.cl>
2025-10-20 01:22:13 -0300

    Creacion de estructura base del proyecto (Login, Inicio, NavBar)

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <NavLink to="/" className="navbar-brand">
+          LEVEL-UP GAMER
+        </NavLink>
+
+        <div>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">
+                Inicio
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/login" className="nav-link">
+                Login
+              </NavLink>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
