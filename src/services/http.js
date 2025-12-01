3c909fb3e2af7fa0f26abf6d3b7b6cbf7a0a659a
JesusAranguiz <jesu.aranguiz@duocuc.cl>
2025-12-01 17:06:21 -0300

    Modificacion para conectar con spring security

--- src/services/http.js
+++ src/services/http.js
@@ -3,17 +3,10 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || '',
   headers: { 'Content-Type': 'application/json' },
-});
-
-// add token automatically if present
-api.interceptors.request.use((config) => {
-  try {
-    const token = localStorage.getItem('token');
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-  } catch (e) {
-    // ignore
+  auth: {
+    username: 'user',
+    password: '1234'
   }
-  return config;
 });
 
 export default api;
